2fc7701d8f8f1bd403f96e2258ef9677271cc0cd
ArdaGurcan <ardagurcan2004@gmail.com>
2023-02-19 01:43:32 -0600

    ending screen

--- Assets/WinGameCameraPan.cs
+++ Assets/WinGameCameraPan.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WinGameCameraPan : MonoBehaviour
 {
     private Vector3 bottom;
     public float speed;
-
+    Image panel;
+    Image img;
+PlayerMovement player;
     // Start is called before the first frame update
      void Awake()
     {
@@ -14,7 +17,10 @@ public class WinGameCameraPan : MonoBehaviour
     PlayerMovement.lastCheckpoint = new Vector3(-2.79f,-7.41f,0);
     }
     void Start()
-    {
+    {panel = transform.GetChild(0).GetChild(0).GetComponent<Image>();
+    Debug.Log(transform.GetChild(0).GetChild(0));
+    img = transform.GetChild(0).GetChild(0).GetChild(0).GetComponent<Image>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
        bottom = new Vector3(0,-3.8900001f,-10);
     
     }
@@ -23,5 +29,11 @@ public class WinGameCameraPan : MonoBehaviour
     void Update()
     {
         transform.position = Vector3.MoveTowards(transform.position, bottom, (speed/1000f));
+        if (player.dead) {
+            if (panel.color.a < 0.7f)
+            panel.color += new Color(0,0,0,Time.deltaTime/3);
+            if (img.color.a < 1f)
+            img.color += new Color(0,0,0,Time.deltaTime/3);
+        }
     }
 }
